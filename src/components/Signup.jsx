abeb8e7809de7f9ad0148334f380d63de655462f
adebayoisrael <adebayoisre004@gmail.com>
2024-11-11 00:56:05 -0800

    third

--- src/components/Signup.jsx
+++ src/components/Signup.jsx
@@ -1,5 +1,8 @@
 import React from 'react'
 import "./Signup.css"
+import { IoMdMail } from "react-icons/io";
+import { FaUserAlt } from "react-icons/fa";
+import { RiLockPasswordFill } from "react-icons/ri";
 
 const Signup = () => {
   return (
@@ -11,19 +14,29 @@ const Signup = () => {
         <form action="" className='forms'>
         <div className="inputs">
      <div className="input">
+      <div className='icons'>
+      <IoMdMail />
+      </div>
+      
         <input type="email" placeholder='Email ID' />
      </div>
      <div className="input">
+     <div className='icons'>
+      <FaUserAlt />
+      </div>
         <input type="text" placeholder='Username' />
      </div>
      <div className="input">
+     <div className='icons'>
+     <RiLockPasswordFill />
+      </div>
         <input type="password"  placeholder='Password'/>
      </div>
         </div>
 
         <div className="submit">
             <div className="sub">
-                <button>
+                <button className='butt'>
                     Sign up
                 </button>
             </div>
